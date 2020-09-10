cb11fe629d6275a64c6c84ef9cba553eab3e6466
kelseyjdickson <kelseymedved@gmail.com>
2020-09-10 18:18:48 -0400

    mvp

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,6 +11,7 @@ class ReviewsController < ApplicationController
 
     def create
         review = Review.create(review_params)
+        
         render json: review.book
     end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,17 @@ class UsersController < ApplicationController
         user = User.find(params[:id])
         render json: user
     end
+
+    def login
+       
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            render json: books
+        else
+            render json: {message: "Incorrect login info! Please try again"}
+        end
+    end
+
+
+    
 end
